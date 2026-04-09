c59d008d904c3066d810228978fe994a216d23ac
isaacjef <isaac.conceicao@estudante.ifgoiano.edu.br>
2026-04-09 09:43:07 -0300

    movimentacao

--- labirinto.py
+++ labirinto.py
@@ -1,3 +1,5 @@
+import os
+
 import numpy as np
 import pygame
 from cons import RECT_COLOR, SQUARE_LENGTH
@@ -9,13 +11,21 @@ class Labirinto:
         self.cores = [pygame.Color(RECT_COLOR), pygame.Color("gray")]
         self.tamanho_quadrado = SQUARE_LENGTH
 
-    def desenhar(self, tela):
+    def desenhar(self, tela, player_xy):
         for linha in range(8):
             for coluna in range(8):
                 cor = self.cores[(linha + coluna) % 2]
-
+                
                 rect = pygame.draw.rect(
                     tela, 
                     cor,
                     (coluna * (self.tamanho_quadrado), linha * (self.tamanho_quadrado), self.tamanho_quadrado, self.tamanho_quadrado)
                 )
+
+                if (linha, coluna) == player_xy:
+                    cor = pygame.Color("red")
+                    railsao = pygame.image.load(os.path.join("world_of_wumpus\\resources","railsao1.png")).convert_alpha()
+                    tela.blit(railsao, rect)
+    
+        
+
--- main.py
+++ main.py
@@ -10,14 +10,41 @@ class Main:
         self.clock = pygame.time.Clock()
         self.labirinto = Labirinto()
 
-    def executar(self):
+    def executar(self, posicao_inicial=(0, 0)):
+        self.posicao_inicial = posicao_inicial
+        
         rodando = True
         while rodando:
             for evento in pygame.event.get():
                 if evento.type == pygame.QUIT:
                     rodando = False
+                if evento.type == pygame.KEYDOWN:
+                    if evento.key == pygame.K_RIGHT and self.posicao_inicial[1] < 7:
+                        print(self.posicao_inicial[1])
+                        self.posicao_inicial = (self.posicao_inicial[0], self.posicao_inicial[1] + 1)
+                    if evento.key == pygame.K_LEFT and self.posicao_inicial[1] > 0:
+                        print(self.posicao_inicial[1])
+                        self.posicao_inicial = (self.posicao_inicial[0], self.posicao_inicial[1] - 1)
+                    if evento.key == pygame.K_DOWN and self.posicao_inicial[0] < 7:
+                        print(self.posicao_inicial[0])
+                        self.posicao_inicial = (self.posicao_inicial[0] + 1, self.posicao_inicial[1])
+                    if evento.key == pygame.K_UP and self.posicao_inicial[0] > 0:
+                        print(self.posicao_inicial[0])
+                        self.posicao_inicial = (self.posicao_inicial[0] - 1, self.posicao_inicial[1])
 
-            self.labirinto.desenhar(self.tela)
+            
+            # teclas = pygame.key.get_pressed()
+            # if teclas[pygame.K_RIGHT] and self.posicao_inicial[1] < 8:
+            #     print(self.posicao_inicial[1])
+            #     self.posicao_inicial = (0, self.posicao_inicial[1] + 1)
+            # if teclas[pygame.K_LEFT] and self.posicao_inicial[1] > 0:
+            #     self.posicao_inicial = (0, self.posicao_inicial[1] - 1)
+            # if teclas[pygame.K_DOWN] and self.posicao_inicial[0] < 8:
+            #     self.posicao_inicial = (self.posicao_inicial[0] + 1, 0)
+            # if teclas[pygame.K_UP] and self.posicao_inicial[0] > 0:
+            #     self.posicao_inicial = (self.posicao_inicial[0] - 1, 0)
+
+            self.labirinto.desenhar(self.tela, self.posicao_inicial)
             
             pygame.display.flip()
             self.clock.tick(60)
