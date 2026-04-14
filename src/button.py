560209bc509cb10f0999bb3c53ed91202197e624
aiefaria <gabribga2002@gmail.com>
2026-04-14 01:13:51 -0300

    Criando novo método paraa destacar imagens semelhante ao changeColor().

--- src/button.py
+++ src/button.py
@@ -1,20 +1,24 @@
 import pygame
 
 class Button():
-	def __init__(self, image, pos, text_input, font, base_color, hovering_color):
+	def __init__(self, image, pos, text_input="", font="qa", base_color="#000000", hovering_color="#000000"):
 		self.image = image
 		self.x_pos = pos[0]
 		self.y_pos = pos[1]
-		self.font = font
+		if font is None or isinstance(font, str):
+			self.font = pygame.font.SysFont("Arial", 20) # Tamanho 20 por padrão
+		else:
+			self.font = font
 		self.base_color, self.hovering_color = base_color, hovering_color
 		self.text_input = text_input
 		self.text = self.font.render(self.text_input, True, self.base_color)
-		
 		if self.image is None:
 			self.image = self.text
 		self.rect = self.image.get_rect(center=(self.x_pos, self.y_pos))
 		self.text_rect = self.text.get_rect(center=(self.x_pos, self.y_pos))
-
+		self.copia = self.image.copy()
+		self.image_destaque = self.image.copy()
+		self.image_destaque.fill((0, 0, 0, 255), special_flags=pygame.BLEND_RGBA_MULT)
 
 	def update(self, screen):
 		if self.image is not None:
@@ -37,3 +41,9 @@ class Button():
 			self.image = image
 		# else:
 		# 	self.text = self.font.render(self.text_input, True, self.base_color)
+	def changeColorImagem(self, position):
+		if position[0] in range(self.rect.left, self.rect.right) and position[1] in range(self.rect.top, self.rect.bottom):
+			self.image = self.image_destaque.copy()
+			print("aqui")
+		else:
+			self.image = self.copia.copy()
