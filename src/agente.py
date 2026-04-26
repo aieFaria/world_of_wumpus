9d9b4aeca6ddb5305555683090181b461d7c8128
aiefaria <gabribga2002@gmail.com>
2026-04-26 15:00:23 -0300

    Somando últimas modificações.

--- src/agente.py
+++ src/agente.py
@@ -1,42 +1,111 @@
 import time
 import pygame
 from labirinto import Labirinto
+from collections import deque
 
 class Agente:
     # Atributos de classe
-    delayMove = 200
+    delayMove = 500
 
     def __init__(self, id):
         # Atributo de instância do agente
         self.id = id
-        self.ponteiro = 0
-        self.last_move_time = 0
+        self.ultimo_move_time = 0
         self.finalizado = False
-        self.visitados = set((3, 3))
-        self.pilha_caminho = [(3, 3)]
+        self.perigosos = set() # Indica quais os locais com certeza agente não deve passar
+        self.fila = deque() # pygame.K_RIGHT, pygame.K_LEFT
+        self.pilha_caminho = [] # Consumida da direta para esquerda, pilha
+
         self.historico = [] # Inicializa vazio
 
-        self.ctrl = {"direcao": "frente", "posicao": (0, 0)}
+        self.labirinto = [] # Matriz flexivel para memória
+
+        self.ctrl = {"direcao": "frente", "localizacao": (0, 0)}
 
         self.leituraLab = {} # Referente ao retorno do labirinto quando agente está ativo
 
     def executar(self):
 
-        current_time = pygame.time.get_ticks()
+        tempo_atual = pygame.time.get_ticks()
+
+        if (not self.leituraLab):
+            self.iniciar()
+            
+        self.tell( self.aux_convertDictBloco(self.leituraLab) )
+
+        # print(self.labirinto)
         
-        if self.leituraLab and not self.finalizado:
-            if current_time - self.last_move_time > self.delayMove:
+        if ( self.leituraLab and not self.finalizado ):
+            if tempo_atual - self.ultimo_move_time > self.delayMove:
+
                 x, y = self.leituraLab.get("bloco")
-                #  = bloco
-                
+
                 if (self.pilha_caminho):
-                    teste = self.pilha_caminho.pop()
+                    destino = self.pilha_caminho.pop()
 
-                    if ((x, y) != teste):
-                        self.movimentacao_segura(x, y, teste)
-                        self.pilha_caminho.append(teste)
+                    if ((x, y) != destino):
+                        self.movimentacao_segura(x, y, destino)
+                        self.pilha_caminho.append(destino)
+                else:
+                    # pensar para atribuir valores a pilha_caminho
+                    # chamada da função ask aqui:
+                    # self.ask()
+                    pass
 
-                self.last_move_time = current_time
+                self.ultimo_move_time = tempo_atual
+
+    """
+    Método ask, deve retornar a posição destino ou a sequencia de 'key' que leva ao destino
+    mudar nome para "pensoLogoExisto" ao final
+    """
+    def ask(self):
+        pass
+    
+    def iniciar(self):
+        # Disposição inicial segura
+        bloco = BlocoI((0, 0), [], False, "", False, False, False, "O")
+        bloco1 = BlocoI((0, 1), [], False, "", False, False, False, "O")
+        bloco2 = BlocoI((1, 0), [], False, "", False, False, False, "O")
+
+        self.tell(bloco)
+        self.tell(bloco1)
+        self.tell(bloco2)
+
+        # Testes da função tell
+        # self.tell( BlocoI((2, 0), [], False, "", False, False, False, "O") )
+        # self.tell( BlocoI((5, 5), [], False, "", False, False, False, "O") ) # Teleport linha e coluna diferente
+        # # self.tell( BlocoI((0, 5), [], False, "", False, False, False, "O") ) # Teleport mesma coluna
+        # # self.tell( BlocoI((5, 0), [], False, "", False, False, False, "O") ) # Teleport mesma linha
+        # self.tell( BlocoI((2, 2), [], False, "", False, False, False, "O") )
+        
+    def tell(self, blocoPercebido): # Talvez uma variavel t para controle do tempo
+
+        x, y = blocoPercebido.posicao
+
+        # Condicional para adicionar o bloco percebido ao labirinto interno
+        # Leva em consideração que não haverá teleport -> Corrigir por conta do morcego
+        contem = any(blocoPercebido in linha for linha in self.labirinto)
+        if( not contem ):
+
+            while len(self.labirinto) <= x:
+                # Adiciona uma nova linha vazia
+                self.labirinto.append([])
+            
+            for i in range(len(self.labirinto)):
+                while len(self.labirinto[i]) <= y:
+                    i = i
+                    j = len(self.labirinto[i])
+                    
+                    if i == x and j == y:
+                        self.labirinto[i].append( blocoPercebido )
+                    else:
+                        self.labirinto[i].append(
+                            BlocoI((i, j), [], False, "", False, False, False)
+                        )
+            
+        else:
+            # Substitui o bloco caso já exista
+            self.labirinto[x][y] = blocoPercebido
 
     def movimentacao_segura(self, x, y, bloco_alvo):
         direcoes = [
@@ -72,5 +141,47 @@ class Agente:
         
     def base_conhecimento(self, atributos):
         for atr in atributos:
-            if (atr == "Breeze/n"):
-                
+            if (atr == "Breeze"):
+                pass
+
+    def aux_convertDictBloco(self, dict):
+        return BlocoI((dict.get('bloco', [-1, -1])), 
+                      dict.get('atributos', []),
+                      dict.get('hasPit'),
+                      dict.get('hasWumpus'),
+                      dict.get('hasBats'),
+                      dict.get('hasArrow'),
+                      dict.get('hasGold'),
+                      "O")
+
+    
+
+# Objeto interno para uso do agente
+class BlocoI: 
+    def __init__(self, posicao, atributos, hasPit, hasWumpus, hasBats, hasArrow, hasGold, ava="?"):
+        self.posicao = posicao
+        self.atributos = atributos
+        self.hasPit = hasPit
+        self.hasWumpus = hasWumpus
+        self.hasBats = hasBats
+        self.hasArrow = hasArrow
+        self.hasGold = hasGold
+
+        # Atributos para uso e classificação do agente:
+        self.perigo = ava # Use: "O" - Seguro | "?" - Dúvida | "X" - Perigosissimo | "P" - Parede
+
+    def __str__(self):
+        return f"{self.posicao}.{self.perigo}"
+    
+    __repr__ = __str__
+    
+    # Equivalente do equals do java
+    def __eq__(self, other):
+        if isinstance(other, BlocoI):
+            return self.posicao == other.posicao
+        return False
+    
+    def setPerigo(self, perigo):
+        self.perigo = perigo
+
+        
