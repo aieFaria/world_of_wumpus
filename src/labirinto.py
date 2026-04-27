3a7ac44d89302a4e1828c58d6d17111cdd349ae0
aiefaria <gabribga2002@gmail.com>
2026-04-27 14:14:16 -0300

    Incluindo condição de vitoria no agente.

--- src/labirinto.py
+++ src/labirinto.py
@@ -20,8 +20,8 @@ class Labirinto:
         self.qtd_flechas = 0 
         self.olhandoWumpus = [] # array de tupla que contém as possibilidades de olhar para wumpus
         # Definição das quantidades de cada coisa no mapa: Configuração padrão Labirinto 6x6
-        self.dic_quantidades = {"morcegos": 2, "ouro": 3, "wumpus": 2, "flecha": 2, "buracos": 4 }
-        # self.dic_quantidades = {"morcegos": 0, "ouro": 3, "wumpus": 0, "flecha": 0, "buracos": 0 }
+        # self.dic_quantidades = {"morcegos": 2, "ouro": 3, "wumpus": 2, "flecha": 2, "buracos": 4 }
+        self.dic_quantidades = {"morcegos": 0, "ouro": 3, "wumpus": 0, "flecha": 0, "buracos": 0 }
         self.morcegos = {"espera": False, "tempo": 0, "posicao": (0, 0)}
         self.jogador_status = 0 # Estados possiveis do jogador: 0 - vivo, 1 - perdeu, 2 - ganhou
         
